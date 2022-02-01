b2b1ac9b7ebfb60cc1e4689493938df45558a6a7
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-02-01 22:21:27 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:calculator/pages/calculator.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.rubik().fontFamily,
+      ),
+      home: const MyCalculator(),
+    );
+  }
+}
